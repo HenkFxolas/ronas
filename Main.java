665e9d7a032ed847584539af67f862ca14a105fd
HenkFxolas <62256511+HenkFxolas@users.noreply.github.com>
2020-03-16 16:45:02 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,22 +12,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TraficLight b = new TraficLight ();
-		System.out.println(b.isGreen());
-		System.out.println(b.isRed());
-		System.out.println(b.pass());
-		System.out.println(b.stop());
-		b.changeColor();
-		System.out.println(b.isYellow());
-		System.out.println(b.pass());
-		b.changeColor();
-		System.out.println(b.isGreen());
-		System.out.println(b.isYellow());
-		System.out.println(b.pass());
-		b.changeColor();
-		System.out.println(b.pass());
-		}
+		PANDELERRO
 	}
 
 
